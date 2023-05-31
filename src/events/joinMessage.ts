d0f640c8ee1bc0b47f1aefe619d843003a34ccd1
AuJung <nuttapong6280@gmail.com>
2023-05-31 19:24:44 +0700

    - remove thumbnail from embed. - add logFunction for joinMessage.ts

--- src/events/joinMessage.ts
+++ src/events/joinMessage.ts
@@ -1,5 +1,5 @@
 import { EmbedBuilder } from 'discord.js'
-import {event} from "../utils";
+import {event, randomColor4Chat} from "../utils";
 import keys from "../keys";
 
 
@@ -15,21 +15,19 @@ export default event('guildMemberAdd', async (
     const member = guildMember.user
     const embed = {
         title: `Welcome to the server, ${member.toString()}`,
-        description: `We're glad you're here!`,
+        description: `We're glad you're here! ${member.tag}. Welcome to our community. We value your presence and are excited to have you join us. Whether you're a new member or a returning one, we want you to know that your presence matters to us.`,
         image: {
             url: member.displayAvatarURL()
         },
         footer: {
             text: `Member #${guildMember.guild.memberCount}`,
-            thumbnail: {
-                url: guildMember.guild.iconURL()
-            }
         },
-        color: 0x00ff00,
+        color: randomColor4Chat(),
     }
     const embedBuilder = new EmbedBuilder(embed)
 
     //send the embed to the channel
     if(!channel.isTextBased()) return log('Channel is not text based')
     await channel.send({embeds: [embedBuilder]})
+    log(`Sent welcome message to ${member.tag}`)
 })
